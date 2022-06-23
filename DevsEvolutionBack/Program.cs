287a481ba880c9aa9149f09fbffdcc5243ebf874
Qw-Arty <artemportnyh@gmail.com>
2022-06-23 21:47:25 +0300

    Conflict master

--- DevsEvolutionBack/Program.cs
+++ DevsEvolutionBack/Program.cs
@@ -31,7 +31,7 @@ app.MapGet("/MyHelloWorld", () =>
     //        summaries[Random.Shared.Next(summaries.Length)]
     //    ))
     //    .ToArray();
-    return "Hello World!!!";
+    return "Hello World!!";
     // get put post delete
 })
 .WithName("GetWeatherForecast");
