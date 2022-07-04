5beb3834ec05b09bc668a3619a637fd820eedc9b
Qw-Arty <artemportnyh@gmail.com>
2022-07-04 11:05:30 +0300

    develop-update

--- DevsEvolutionBack/Program.cs
+++ DevsEvolutionBack/Program.cs
@@ -25,6 +25,18 @@ builder.Services.AddMvc();
 
 var app = builder.Build();
 
+if (app.Request.HttpMethod == "OPTIONS")
+{
+    app.Response.StatusCode = 200;
+    app.Response.AddHeader("Access-Control-Allow-Headers", "content-type,accept,authorization");
+    app.Response.AddHeader("Access-Control-Allow-Origin", "*");
+    app.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+    app.Response.AddHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE");
+    app.Response.AddHeader("Content-Type", "application/json");
+    app.Response.AddHeader("Accept", "application/json");
+    app.Response.End();
+}
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
